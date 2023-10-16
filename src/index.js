d0dd6aa3b7ff4dc48a4b0926cf7a79fa4bfb31cb
Yeider <yeiderjp12@gmail.com>
2023-10-16 18:49:41 -0500

    Se añadio una configuracion del CORS

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ import express from 'express'
 import { pool } from './DBpg.js'
 import { Port } from './config.js'
 import fs from 'fs/promises'
+import cors from 'cors'
 
 const app = express()
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Cambia a tu dominio
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-})
+// Manejo de CORS de manera manual
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:5172'); // Cambia a tu dominio
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// })
+
+const originsCors = ['http://localhost:5173', 'https://rutine-editor.vercel.app/']
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (originsCors.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,POST',
+  optionsSuccessStatus: 204,
+}; 
+
+app.use(cors(corsOptions))
 
 app.get('/main', async(req, res)=>{
   const search = req.query.food
@@ -25,62 +43,62 @@ app.get('/main', async(req, res)=>{
 
 })
 
-app.get('/addTable', async(req, res) => {
+// app.get('/addTable', async(req, res) => {
 
-  try {
-    const query = `
-      CREATE TABLE alimento (
-      id_alimento serial NOT NULL constraint pk_alimento primary key, 
-      nombre character varying(100) not null, 
-      calorias numeric(5, 1) not null, 
-      proteinas numeric(4, 1) not null, 
-      carbohidratos numeric(4, 1) not null, 
-      grasas numeric(4, 1) not null
-      )
-    `
-    await pool.query(query);
+//   try {
+//     const query = `
+//       CREATE TABLE alimento (
+//       id_alimento serial NOT NULL constraint pk_alimento primary key, 
+//       nombre character varying(100) not null, 
+//       calorias numeric(5, 1) not null, 
+//       proteinas numeric(4, 1) not null, 
+//       carbohidratos numeric(4, 1) not null, 
+//       grasas numeric(4, 1) not null
+//       )
+//     `
+//     await pool.query(query);
 
-    res.status(200).send("operacion realizada con exito")
-  } catch (error) {
-    console.error("Error al crear la tabla en la base de datos", error);
-    res.status(500).send("Error al crear la tabla")
-  }
-})
+//     res.status(200).send("operacion realizada con exito")
+//   } catch (error) {
+//     console.error("Error al crear la tabla en la base de datos", error);
+//     res.status(500).send("Error al crear la tabla")
+//   }
+// })
 
-app.get('/pushData', async (req, res) => {
+// app.get('/pushData', async (req, res) => {
 
-  try {
-    const data = await fs.readFile('src/alimentos.json', 'utf8')
-    const alimentos = JSON.parse(data)
+//   try {
+//     const data = await fs.readFile('src/alimentos.json', 'utf8')
+//     const alimentos = JSON.parse(data)
 
     
-    for ( const element of alimentos) {
-      const {nombre, calorias, proteinas, carbohidratos, grasas} = element
-      const values = [nombre, parseFloat(calorias), parseFloat(proteinas), parseFloat(carbohidratos), parseFloat(grasas)]
-      const query = `insert into alimento(nombre, calorias, proteinas, carbohidratos, grasas) values($1, $2, $3, $4, $5);`
-      await pool.query(query, values)
-    };
+//     for ( const element of alimentos) {
+//       const {nombre, calorias, proteinas, carbohidratos, grasas} = element
+//       const values = [nombre, parseFloat(calorias), parseFloat(proteinas), parseFloat(carbohidratos), parseFloat(grasas)]
+//       const query = `insert into alimento(nombre, calorias, proteinas, carbohidratos, grasas) values($1, $2, $3, $4, $5);`
+//       await pool.query(query, values)
+//     };
 
-    res.status(200).send("Datos insertados correctamente")
+//     res.status(200).send("Datos insertados correctamente")
     
-  } catch (error) {
-    console.error("Error al insertar los datos", error)
-    res.status(500).send("Error al insertar los datos")
-  }
-})
+//   } catch (error) {
+//     console.error("Error al insertar los datos", error)
+//     res.status(500).send("Error al insertar los datos")
+//   }
+// })
 
-app.get('/deleteTable', async (req, res) => {
+// app.get('/deleteTable', async (req, res) => {
 
-  try {
-    const query = `drop table if exists ${req.params.table || 'tabla'}`;
-    await pool.query(query)
-    res.status(200).send("Se elimino la tabla con exito")
+//   try {
+//     const query = `drop table if exists ${req.params.table || 'tabla'}`;
+//     await pool.query(query)
+//     res.status(200).send("Se elimino la tabla con exito")
 
-  } catch (error) {
-    console.error("Error al eliminar la tabla", error)
-    res.status(500).send("Error al eliminar la tabla")
-  }
-})
+//   } catch (error) {
+//     console.error("Error al eliminar la tabla", error)
+//     res.status(500).send("Error al eliminar la tabla")
+//   }
+// })
 
 app.listen(Port);
 console.log("Server in port", Port);
